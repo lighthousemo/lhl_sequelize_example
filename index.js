598258a096d8c2e2dce422f56186f9f11f96fc3a
Monica Olinescu <monica.olinescu@gmail.com>
2016-09-02 09:51:54 -0700

    Add gitignore

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var express = require("express");
 var app = express();
 var config = require("./config");
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // default is /views folder
 
 app.get("/", (req, res) => {
   // render the views/index.ejs file
   res.render("index", {greeting: "Hello there!"})
 });
 
+// start server
 app.listen(config.port, () => {
   console.log(`Example app listening on port ${config.port}!`);
 });
