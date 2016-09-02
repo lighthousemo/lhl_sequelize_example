f9c6ba268fd1aad3c2a3abb4bf05ea71dfdabe77
Monica Olinescu <monica.olinescu@gmail.com>
2016-09-02 11:01:39 -0700

    Set up sequelize. Add User model.

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 var express = require("express");
 var app = express();
 var config = require("./config");
+var db = require("./models/index");
 
 app.set("view engine", "ejs"); // default is /views folder
 
 app.get("/", (req, res) => {
-  // render the views/index.ejs file
-  res.render("index", {greeting: "Hello there!"})
+  db.User.findAll()
+  .then((users) => {
+    res.render("index", {users})
+    // line above is the same as
+    // res.render("index", {users: users})
+  });
+});
+
+// Create a user with an email address that starts with NAME
+// GET /test_create/NAME
+app.get("/test_create/:name", (req, res) => {
+  db.User.create({email: `${req.params.name || "bob"}@gmail.com`})
+  .then((user) => {
+    res.redirect("/");
+  })
+  .catch((err) => {
+    res.send("There was an error: " + err);
+  })
 });
 
 // start server
